3d41e34dc2e748b4c367bfb82c90b5912a6f1cb7
greg4fun <gstencel@gmail.com>
2023-12-07 14:04:40 +0000

    Nikola auto commit.
    Nikola version: 8.2.4

---
+++ cache/.mako.tmp/base.tmpl.py
@@ -0,0 +1,175 @@
+# -*- coding:utf-8 -*-
+from mako import runtime, filters, cache
+UNDEFINED = runtime.UNDEFINED
+STOP_RENDERING = runtime.STOP_RENDERING
+__M_dict_builtin = dict
+__M_locals_builtin = locals
+_magic_number = 10
+_modified_time = 1701957879.3499544
+_enable_loop = True
+_template_filename = 'themes/zen/templates/base.tmpl'
+_template_uri = 'base.tmpl'
+_source_encoding = 'utf-8'
+_exports = ['extra_head', 'content', 'extra_js']
+
+
+def _mako_get_namespace(context, name):
+    try:
+        return context.namespaces[(__name__, name)]
+    except KeyError:
+        _mako_generate_namespaces(context)
+        return context.namespaces[(__name__, name)]
+def _mako_generate_namespaces(context):
+    ns = runtime.TemplateNamespace('base', context._clean_inheritance_tokens(), templateuri='base_helper.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'base')] = ns
+
+    ns = runtime.TemplateNamespace('header', context._clean_inheritance_tokens(), templateuri='base_header.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'header')] = ns
+
+    ns = runtime.TemplateNamespace('footer', context._clean_inheritance_tokens(), templateuri='base_footer.tmpl', callables=None,  calling_uri=_template_uri)
+    context.namespaces[(__name__, 'footer')] = ns
+
+def render_body(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        __M_locals = __M_dict_builtin(pageargs=pageargs)
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
+        messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
+        _link = _import_ns.get('_link', context.get('_link', UNDEFINED))
+        base = _mako_get_namespace(context, 'base')
+        template_hooks = _import_ns.get('template_hooks', context.get('template_hooks', UNDEFINED))
+        momentjs_locales = _import_ns.get('momentjs_locales', context.get('momentjs_locales', UNDEFINED))
+        js_date_format = _import_ns.get('js_date_format', context.get('js_date_format', UNDEFINED))
+        lang = _import_ns.get('lang', context.get('lang', UNDEFINED))
+        logo_url = _import_ns.get('logo_url', context.get('logo_url', UNDEFINED))
+        abs_link = _import_ns.get('abs_link', context.get('abs_link', UNDEFINED))
+        date_fanciness = _import_ns.get('date_fanciness', context.get('date_fanciness', UNDEFINED))
+        body_end = _import_ns.get('body_end', context.get('body_end', UNDEFINED))
+        def extra_head():
+            return render_extra_head(context._locals(__M_locals))
+        search_form = _import_ns.get('search_form', context.get('search_form', UNDEFINED))
+        set_locale = _import_ns.get('set_locale', context.get('set_locale', UNDEFINED))
+        blog_title = _import_ns.get('blog_title', context.get('blog_title', UNDEFINED))
+        def extra_js():
+            return render_extra_js(context._locals(__M_locals))
+        footer = _mako_get_namespace(context, 'footer')
+        def content():
+            return render_content(context._locals(__M_locals))
+        __M_writer = context.writer()
+        __M_writer('\n')
+        __M_writer('\n')
+        __M_writer('\n')
+        __M_writer(str(set_locale(lang)))
+        __M_writer('\n')
+        __M_writer(str(base.html_headstart()))
+        __M_writer('\n')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'extra_head'):
+            context['self'].extra_head(**pageargs)
+        
+
+        __M_writer('\n')
+        __M_writer(str(template_hooks['extra_head']()))
+        __M_writer('\n</head>\n<body>\n    <a href="#page-content" class="sr-only sr-only-focusable">')
+        __M_writer(str(messages("Skip to main content")))
+        __M_writer('</a>\n    ')
+        __M_writer(str(template_hooks['page_header']()))
+        __M_writer('\n    <section class="social">\n        <ul>\n        <li><p style="color:#fff;font-size:3em;">stencel.io</p></li>\n        <li><p style="color:#fff;font-size:2em;">greg\'s tech notes</p></li>\n')
+        if logo_url:
+            __M_writer('            <li>\n                <a href="')
+            __M_writer(str(abs_link(_link("root", None, lang))))
+            __M_writer('">\n                    <img src="')
+            __M_writer(str(logo_url))
+            __M_writer('" alt="')
+            __M_writer(filters.html_escape(str(blog_title)))
+            __M_writer('" id="logo">\n                </a>\n            </li>\n')
+        __M_writer('        ')
+        __M_writer(str(base.html_navigation_links()))
+        __M_writer('\n')
+        if search_form:
+            __M_writer('            <div class="searchform" role="search">\n                ')
+            __M_writer(str(search_form))
+            __M_writer('\n            </div>\n')
+        __M_writer('        </ul>\n    </section>\n    <section class="page-content">\n        <div class="content" rel="main">\n            ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
+            context['self'].content(**pageargs)
+        
+
+        __M_writer('\n             ')
+        __M_writer(str(footer.html_footer()))
+        __M_writer('\n        </div>\n    </section>\n    ')
+        __M_writer(str(base.late_load_js()))
+        __M_writer('\n    <!-- fancy dates -->\n    <script>\n    moment.locale("')
+        __M_writer(str(momentjs_locales[lang]))
+        __M_writer('");\n    fancydates(')
+        __M_writer(str(date_fanciness))
+        __M_writer(', ')
+        __M_writer(str(js_date_format))
+        __M_writer(');\n    </script>\n    <!-- end fancy dates -->\n    ')
+        if 'parent' not in context._data or not hasattr(context._data['parent'], 'extra_js'):
+            context['self'].extra_js(**pageargs)
+        
+
+        __M_writer("\n    <script>\n    baguetteBox.run('div#content', {\n        ignoreClass: 'islink',\n        captions: function(element) {\n            return element.getElementsByTagName('img')[0].alt;\n    }});\n    </script>\n    ")
+        __M_writer(str(body_end))
+        __M_writer('\n    ')
+        __M_writer(str(template_hooks['body_end']()))
+        __M_writer('\n</body>\n</html>\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_extra_head(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
+        def extra_head():
+            return render_extra_head(context)
+        __M_writer = context.writer()
+        __M_writer('\n')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_content(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
+        def content():
+            return render_content(context)
+        __M_writer = context.writer()
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+def render_extra_js(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        _import_ns = {}
+        _mako_get_namespace(context, 'base')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'header')._populate(_import_ns, ['*'])
+        _mako_get_namespace(context, 'footer')._populate(_import_ns, ['*'])
+        def extra_js():
+            return render_extra_js(context)
+        __M_writer = context.writer()
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
+"""
+__M_BEGIN_METADATA
+{"filename": "themes/zen/templates/base.tmpl", "uri": "base.tmpl", "source_encoding": "utf-8", "line_map": {"23": 2, "26": 3, "29": 4, "32": 0, "62": 2, "63": 3, "64": 4, "65": 5, "66": 5, "67": 7, "68": 7, "73": 10, "74": 11, "75": 11, "76": 14, "77": 14, "78": 15, "79": 15, "80": 20, "81": 21, "82": 22, "83": 22, "84": 23, "85": 23, "86": 23, "87": 23, "88": 27, "89": 27, "90": 27, "91": 28, "92": 29, "93": 30, "94": 30, "95": 33, "100": 37, "101": 38, "102": 38, "103": 41, "104": 41, "105": 44, "106": 44, "107": 45, "108": 45, "109": 45, "110": 45, "115": 48, "116": 56, "117": 56, "118": 57, "119": 57, "125": 8, "135": 8, "141": 37, "156": 48, "171": 156}}
+__M_END_METADATA
+"""
